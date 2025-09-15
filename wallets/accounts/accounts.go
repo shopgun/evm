719527343ddef0f1b0be8fd26621153ae5b984e6
shopgun <666abhishek666@gmail.com>
2025-09-15 20:02:09 +0530

    Update accounts.go

--- wallets/accounts/accounts.go
+++ wallets/accounts/accounts.go
@@ -19,6 +19,7 @@ type Account struct {
 // accounts (derived from the same seed).
 type Wallet interface {
 	// URL retrieves the canonical path under which this wallet is reachable. It is
+	// a test
 	// used by upper layers to define a sorting order over all wallets from multiple
 	// backends.
 	URL() gethaccounts.URL
